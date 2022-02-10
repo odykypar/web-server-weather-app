3157c9121245eeb41fbb2971fd3ebb4772cfc22f
odyskypa <odisseaskip@gmail.com>
2022-02-10 16:45:03 +0200

    Changing name

--- src/app.js
+++ src/app.js
@@ -25,14 +25,14 @@ app.use(express.static(publicDirectoryPath))
 app.get('', (req, res) => {
     res.render('index', {
         title: 'Weather',
-        name: 'Andrew Mead'
+        name: 'Odysseas Kyparissis'
     })
 })
 
 app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
-        name: 'Andrew Mead'
+        name: 'Odysseas Kyparissis'
     })
 })
 
@@ -40,7 +40,7 @@ app.get('/help', (req, res) => {
     res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
-        name: 'Andrew Mead'
+        name: 'Odysseas Kyparissis'
     })
 })
 
